ef228b7464ae6561c22e4d6a3a32b3dd41ae5b59
Artur Poffo <arturpoffoc@hotmail.com>
2023-09-22 21:03:31 -0300

    fix: footer link

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="w-full bg-brand-gray-600 opacity-80 p-5 flex items-center justify-center gap-4" >
       <span className="font-mono font-bold" >Desenvolvido por: Artur Poffo</span>
       <strong>|</strong>
-      <Link className="font-mono font-bold underline underline-offset-4" href={"https://Artur-Poffo"} >Código fonte no GitHub</Link>
+      <Link className="font-mono font-bold underline underline-offset-4" href={"https://github.com/Artur-Poffo/Portfolio"} >Código fonte no GitHub</Link>
     </footer>
   )
 }
